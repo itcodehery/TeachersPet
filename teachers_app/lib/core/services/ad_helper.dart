c04bbe23b6f1b1369f8cfd1b000b1ffe21b459de
Hari Prasad <haririo321@gmail.com>
2026-08-16 23:34:47 +0530

    added ads

--- teachers_app/lib/core/services/ad_helper.dart
+++ teachers_app/lib/core/services/ad_helper.dart
@@ -5,7 +5,7 @@ class AdHelper {
     if (Platform.isAndroid) {
       return 'ca-app-pub-1667720226572708/6158072287';
     } else if (Platform.isIOS) {
-      throw UnsupportedError('Unsupported platform');
+      return 'ca-app-pub-3940256099942544/2934735716';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -15,7 +15,7 @@ class AdHelper {
     if (Platform.isAndroid) {
       return 'ca-app-pub-1667720226572708/6015077412';
     } else if (Platform.isIOS) {
-      throw UnsupportedError('Unsupported platform');
+      return 'ca-app-pub-3940256099942544/4411468910';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -25,7 +25,7 @@ class AdHelper {
     if (Platform.isAndroid) {
       return 'ca-app-pub-1667720226572708/4869516702';
     } else if (Platform.isIOS) {
-      throw UnsupportedError('Unsupported platform');
+      return 'ca-app-pub-3940256099942544/1712485313';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
